11bfc2c9f865e9df1fbf673cecf8ffd7580bd7de
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-01-10 06:54:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = Vue.createApp({
   data() {
     return {
       firstName: 'Chan',
+      lastName: 'Chan',
+      email: 'h951115951115@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg',
     }
   },
 })
